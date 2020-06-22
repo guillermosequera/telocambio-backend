4fa0ec7f71eb4c132385a2916c37ebccb33789c2
guillermosequera <sequeracrespo@gmail.com>
2020-06-22 19:51:08 -0400

    addmigrate

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
 import os
 load_dotenv()
 #incio de la app
@@ -15,6 +16,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # inicio base de datos
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 #incio marshmallow
 ma = Marshmallow(app)
@@ -22,7 +24,7 @@ ma = Marshmallow(app)
 #Producto Class/Model
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
+    name = db.Column(db.String(100), unique=False)
     description = db.Column(db.String(200))
     price = db.Column(db.Float)
     qty = db.Column(db.Integer)
@@ -56,7 +58,7 @@ def add_product():
     db.session.add(new_product)
     db.session.commit()
 
-    return product_schema.jsonify(new_product)
+    return jsonify({"id":new_product["id"]})
 #servidor
 
     if __name__ == '_main_':
