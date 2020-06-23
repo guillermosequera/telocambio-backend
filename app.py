e6b920953b1be2e7da351b6ae7e017f096a99c29
patodekaz <pato@dekaz.cl>
2020-06-23 16:03:32 -0400

    modelos usuario

--- app.py
+++ app.py
@@ -21,6 +21,22 @@ migrate = Migrate(app, db)
 #incio marshmallow
 ma = Marshmallow(app)
 
+#user Class/Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100))
+    lastname = db.Column(db.String(200))
+    email = db.Column(db.String(200))
+    password = db.Column(db.String(200))
+    role = db.Column(db.String(100))
+
+    def __init__(self, firstname, lastname, email, password, role):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.password = password
+        self.role = role
+
 #Producto Class/Model
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -41,11 +57,37 @@ class ProductSchema(ma.Schema):
     class Meta:
         fields = ('id', 'name', 'description', 'price', 'qty')
 
-# inicio Schema
+# Esquema de usuario
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'firstname', 'lastname', 'email', 'password' , 'role')
+
+# inicio Schema PRODUCTO
 product_schema = ProductSchema()
 products_schemas = ProductSchema(many=True)
-# Crea un Producto
 
+# inicio Schema USUARIO
+user_schema = UserSchema()
+users_schemas = UserSchema(many=True)
+
+
+# Crea un USUARIO
+@app.route('/user', methods=['POST'])
+def add_user():
+    firstname = request.json['firstname']
+    lastname = request.json['lastname']
+    email = request.json['email']
+    password = request.json['password']
+    role = request.json['role']
+
+    new_user = User(firstname, lastname, email, password, role)
+
+    db.session.add(new_user)
+    db.session.commit()
+    dump_data = user_schema.dump(new_user)
+    return dump_data
+
+# Crea un Producto
 @app.route('/product', methods=['POST'])
 def add_product():
     name = request.json['name']
@@ -58,7 +100,6 @@ def add_product():
     db.session.add(new_product)
     db.session.commit()
     dump_data = product_schema.dump(new_product)
-
     return dump_data
 
 # Obteniendo todos los productos
@@ -67,6 +108,14 @@ def get_products():
     all_products = Product.query.all()
     result = products_schemas.dump(all_products)
     return jsonify(result)
+
+# Obteniendo todos los usuarios
+@app.route('/users', methods=['GET'])
+def get_users():
+    all_users = User.query.all()
+    result = users_schemas.dump(all_products)
+    return jsonify(result)
+    
 
 # Obteniendo un producto
 @app.route('/product/<id>', methods=['GET'])
@@ -88,7 +137,6 @@ def update_product(id):
     product.description = description
     product.price = price
     product.qty = qty
-
     
     db.session.commit()
     dump_data = product_schema.dump(product)
