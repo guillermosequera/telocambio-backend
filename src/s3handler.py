ee2d07365e6577e4e55c18100475b9e5990d0562
patodekaz <pato@dekaz.cl>
2020-07-29 06:54:35 -0400

    aws handler cred

--- src/s3handler.py
+++ src/s3handler.py
@@ -3,9 +3,12 @@ import os
 from datetime import datetime 
 s3_client = boto3.client(
     "s3",
-    aws_access_key_id=os.environ.get('S3_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.environ.get('S3_ACCESS_SECRET_KEY')
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
 )
+
+session = boto3.Session( aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+s3 = session.resource('s3')
 
 def s3upload(file):
     split_filename = file.filename.split('.')
@@ -13,6 +16,6 @@ def s3upload(file):
     seq = dt.strftime("%m%d%Y-%H%M%S%f")
     finalfilename = split_filename[0] + '_' + seq + '.' + split_filename[1]
     s3_resource = boto3.resource('s3')
-    my_bucket = s3_resource.Bucket(os.environ.get('S3_BUCKET_NAME'))
+    my_bucket = s3_resource.Bucket(os.environ.get('AWS_BUCKET_NAME'))
     my_bucket.Object(finalfilename).put(Body=file, ACL='public-read',ContentType='image/jpeg')
-    return f"https://{os.environ.get('S3_BUCKET_NAME')}.s3-us-west-2.amazonaws.com/{finalfilename}"
+    return f"https://{os.environ.get('AWS_BUCKET_NAME')}.s3-us-west-2.amazonaws.com/{finalfilename}"
